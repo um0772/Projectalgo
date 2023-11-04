29e73c21b8283b93f7a4fa31ae231a72b0c3b84b
Suk <ops0044@naver.com>
2023-11-04 13:26:07 +0900

    commit

--- Projectalgo/main.c
+++ Projectalgo/main.c
@@ -1,51 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 9 //최대 정점의 수를 9개로 지정
-#define MAX_SIZE 100
-#define INF 999999
+#define MAX_VERTICES 9 // 최대 정점의 수를 9로 정의합니다.
+#define MAX_SIZE 100 // 최대 힙 크기를 100으로 정의합니다.
+#define INF 999999 // 무한대를 나타내는 값으로 999999를 사용합니다.
 
 typedef struct Graph {
-    int V; //정점
-    int W; //가중치
-    struct Graph* link;
+    int V; // 정점을 나타냅니다.
+    int W; // 해당 정점의 가중치를 나타냅니다.
+    struct Graph* link; // 다음 인접 정점을 가리키는 포인터입니다.
 
 }Graph;
 
 typedef struct inputGraph {
-    int a;  //총 정점의 개수
-    Graph* adjList[MAX_VERTICES];
+    int a; // 그래프의 총 정점 수를 나타냅니다.
+    Graph* adjList[MAX_VERTICES]; // 인접 리스트 입니다.
 
 }inputGraph;
 
 typedef struct Heap {
-    int heap;
-    int size[MAX_SIZE];
-    int distance[MAX_SIZE];
+    int heap; // 힙에 저장된 현재 요소의 수입니다.
+    int size[MAX_SIZE]; // 힙의 크기를 저장하는 배열입니다.
+    int distance[MAX_SIZE]; // 각 정점까지의 거리를 저장하는 배열입니다.
+
 }Heap;
 
 Heap* init_Heap() {
-    Heap* h = (Heap*)malloc(sizeof(Heap));
+    Heap* h = (Heap*)malloc(sizeof(Heap)); // 힙을 동적 할당합니다.
     if (h == NULL) {
-        // 메모리 할당 실패 처리
+        // 메모리 할당에 실패한 경우 프로그램을 종료합니다.
         exit(1);
     }
-    h->heap = 0;
+    h->heap = 0; // 힙의 초기 요소 수를 0으로 설정합니다.
     for (int i = 0; i < MAX_SIZE; i++) {
-        h->distance[i] = INF;  // 처음에는 모든 거리를 무한대로 설정
+        h->distance[i] = INF; // 거리 배열을 무한대로 초기화합니다.
     }
-    return h;
+    return h; // 초기화된 힙을 반환합니다.
 }
 
 void add_heap(Heap* h, int size, int dist) {
-    h->heap += 1;
-    h->size[h->heap] = size;
-    h->distance[size] = dist;
+    h->heap += 1; // 힙에 요소를 하나 추가합니다.
+    h->size[h->heap] = size; // 힙의 마지막 위치에 새 요소를 추가합니다.
+    h->distance[size] = dist; // 해당 요소의 거리 값을 업데이트합니다.
 
-    int child = h->heap;
-    int parent = child / 2;
+    int child = h->heap; // 추가된 요소의 위치입니다.
+    int parent = child / 2; // 부모 요소의 위치입니다.
 
     while (parent) {
+        // 자식이 부모보다 작으면 스왑합니다.
         if (h->distance[h->size[parent]] > h->distance[h->size[child]]) {
             int n = h->size[parent];
             h->size[parent] = h->size[child];
@@ -54,97 +56,117 @@ void add_heap(Heap* h, int size, int dist) {
             parent = child / 2;
         }
         else
-            break;
+            break; // 부모가 자식보다 작거나 같으면 반복을 중단합니다.
     }
 }
 
 int empty(Heap* h) {
+    // 힙이 비어있으면 1을, 그렇지 않으면 0을 반환합니다.
     return (h->heap ? 0 : 1);
 }
 
 int delete_min_heap(Heap* h) {
     if (h->heap == 0) {
+        // 힙이 비어있으면 오류 메시지를 출력하고 -1을 반환합니다.
         printf("heap empty\n");
         return -1;
     }
-    int root = h->size[1];
-    h->size[1] = h->size[h->heap];
-    h->distance[root] = INF;  // 제거된 정점의 거리는 무한대로 설정
-    h->heap--;
+    int root = h->size[1]; // 힙의 루트 요소(가장 작은 값)를 가져옵니다.
+    h->size[1] = h->size[h->heap]; // 힙의 마지막 요소를 루트로 이동합니다.
 
-    int parent = 1;
-    int child = 2;
+    h->heap--; // 힙 크기를 1 감소시킵니다.
+
+    int parent = 1; // 루트부터 시작합니다.
+    int child = 2 * parent; // 루트의 자식 위치입니다.
 
     while (child <= h->heap) {
+        // 오른쪽 자식이 있고, 왼쪽 자식보다 작으면 오른쪽 자식을 선택합니다.
         if (child + 1 <= h->heap && h->distance[h->size[child]] > h->distance[h->size[child + 1]]) {
-            child += 1;
+            child++;
         }
+        // 부모보다 작은 자식을 부모와 교환합니다.
         if (h->distance[h->size[parent]] > h->distance[h->size[child]]) {
-            int n = h->size[parent];
+            int temp = h->size[parent];
             h->size[parent] = h->size[child];
-            h->size[child] = n;
+            h->size[child] = temp;
 
             parent = child;
-            child = parent * 2;
+            child = 2 * parent;
         }
-        else
-            break;
+        else {
+            break; // 부모가 자식보다 작거나 같으면 반복을 중단합니다.
+        }
     }
-    return root;
+    return root; // 제거된 루트 요소를 반환합니다.
 }
 
 void init_adjList(inputGraph* g) {
-    int i;
-    g->a = 0;
-    for (i = 0; i <= MAX_VERTICES; i++) {  //MAX_VERTICES가 9으로 설정되어 있으므로 0부터 9까지 하나씩 증가시키면서 정점의 인접 리스트를 NULL로 초기화 한다.
-        g->adjList[i] = NULL;
+    g->a = 0; // 그래프의 정점 수를 0으로 초기화합니다.
+    for (int i = 0; i < MAX_VERTICES; i++) {
+        g->adjList[i] = NULL; // 인접 리스트를 NULL로 초기화합니다.
     }
 }
-
 void add_vertex(inputGraph* g, int v) {
     if ((g->a) >= MAX_VERTICES) {
+        // 정점의 수가 최대 정점 수를 초과하면 오류 메시지를 출력하고 함수를 종료합니다.
         fprintf(stderr, "정점 개수 초과");
         return;
     }
-    g->a++;
+    g->a++; // 그래프의 정점 수를 1 증가시킵니다.
 }
 
+
 void add_edge(inputGraph* g, int u, int v, int w) {
-    if (u >= g->a || v >= g->a) {
-        fprintf(stderr, "간선 오류");
+    if (u >= MAX_VERTICES || v >= MAX_VERTICES) {
+        fprintf(stderr, "간선 오류"); // u 또는 v가 최대 정점 수를 초과하면 오류 메시지를 출력합니다.
         return;
     }
-    Graph* n = (Graph*)malloc(sizeof(Graph));
-    if (n == NULL) {
-        fprintf(stderr, "메모리 할당 실패");
+    Graph* n1 = (Graph*)malloc(sizeof(Graph)); // u에서 v로 가는 간선을 위한 메모리를 할당합니다.
+    Graph* n2 = (Graph*)malloc(sizeof(Graph)); // v에서 u로 가는 간선을 위한 메모리를 할당합니다.
+    if (n1 == NULL || n2 == NULL) {
+        fprintf(stderr, "메모리 할당 실패"); // 메모리 할당에 실패하면 오류 메시지를 출력하고 프로그램을 종료합니다.
         exit(1);
     }
-    n->V = v;
-    n->W = w;
-    n->link = g->adjList[u];
-    g->adjList[u] = n;
+    // u에서 v로의 간선 추가
+    n1->V = v;
+    n1->W = w;
+    n1->link = g->adjList[u];
+    g->adjList[u] = n1;
+
+    // v에서 u로의 간선 추가, 무방향 그래프이기 때문에 양쪽으로 추가해줍니다.
+    n2->V = u;
+    n2->W = w;
+    n2->link = g->adjList[v];
+    g->adjList[v] = n2;
 }
 
 void print_adjList(inputGraph* g) {
-    printf("그래프 출력");
-    printf("\n\n");
+    printf("-------인접 리스트를 사용한 그래프 출력---------\n\n");
     for (int i = 0; i < g->a; i++) {
         Graph* pg = g->adjList[i];
-        printf("정점 %d의 인접리스트: ", i);
+        printf("정점 %d: ", i);
         while (pg != NULL) {
-            printf("%d -> %d (가중치: %d) \t",i, pg->V, pg->W);
+            if (i < pg->V) {
+                // 간선과 가중치를 출력합니다.
+                printf("%d -> %d (가중치: %d) \t", i, pg->V, pg->W);
+            }
             pg = pg->link;
         }
         printf("\n");
     }
+    printf("\n");
+    printf("----------------- key 값 출력 ------------------\n\n");
 }
 
-void decrease_key(Heap* h, int v, int distance[]) {
-    h->distance[v] = distance[v];  // 힙의 거리 값을 갱신
+void decrease_key(Heap* h, int v, int* distance) {
+    int prevDistance = h->distance[v]; // 변경 전의 거리 값을 저장합니다.
+
+    h->distance[v] = distance[v];  // 힙의 거리 값을 갱신합니다.
     for (int i = 1; i <= h->heap; i++) {
         if (h->size[i] == v) {
             int child = i;
             int parent = i / 2;
+            // 부모와 자식의 거리 값을 비교하여, 필요한 경우 위치를 교환합니다.
             while (parent) {
                 if (h->distance[h->size[parent]] > h->distance[h->size[child]]) {
                     int n = h->size[parent];
@@ -153,69 +175,80 @@ void decrease_key(Heap* h, int v, int distance[]) {
                     child = parent;
                     parent = child / 2;
                 }
-                else
+                else {
                     break;
+                }
+            }
+            // 거리 값이 변경되었으면, 변경된 거리 값을 출력합니다.
+            if (prevDistance != h->distance[v]) {
+                printf("정점 %d의 가중치가 %d에서 %d로 변경되었습니다.\n", v, prevDistance, h->distance[v]);
             }
             break;
         }
     }
 }
 
+// 프림 알고리즘 구현
 void Prim(inputGraph* g, int start) {
-    int distance[MAX_VERTICES];
-    int nearest[MAX_VERTICES];
-    int MST[MAX_VERTICES] = { 0, };  // false로 초기화
-    Heap* heap = init_Heap();
-    int totalWeight = 0; // 추가: 전체 가중치를 저장하기 위한 변수
+    int distance[MAX_VERTICES]; // 각 정점까지의 거리를 저장하는 배열
+    int nearest[MAX_VERTICES]; // 가장 가까운 정점을 저장하는 배열
+    int MST[MAX_VERTICES] = { 0, }; // 최소 신장 트리에 포함된 정점을 표시하는 배열
+    Heap* heap = init_Heap(); // 최소 힙을 초기화합니다.
+    int totalWeight = 0; // 최소 신장 트리의 총 가중치
 
+    // 모든 정점에 대해 거리를 무한대로 초기화하고, 최소 힙에 추가합니다.
     for (int i = 0; i < g->a; i++) {
         distance[i] = INF;
         nearest[i] = start;
         add_heap(heap, i, distance[i]);
     }
+    // 시작 정점의 거리를 0으로 설정하고 키 값을 감소시킵니다.
     distance[start] = 0;
     decrease_key(heap, start, distance);
 
-    for (int i = 0; i < g->a - 2; i++) {  // n-2번 반복
-        int u = delete_min_heap(heap);
-        MST[u] = 1;  // true로 설정
+    // 모든 정점을 순회하며 최소 신장 트리를 구성합니다.
+    for (int i = 0; i < g->a - 1; i++) {
+        int u = delete_min_heap(heap); // 최소 힙에서 최소 거리 정점을 추출합니다.
+        MST[u] = 1; // 추출된 정점을 최소 신장 트리에 포함시킵니다.
 
+        // 추출된 정점과 인접한 정점들에 대해 거리를 갱신합니다.
         for (Graph* v = g->adjList[u]; v != NULL; v = v->link) {
             int w = v->V;
-            if (!MST[w]) {
-                if (v->W < distance[w]) {
-                    distance[w] = v->W;
-                    nearest[w] = u;
-                    decrease_key(heap, w, distance);
-                }
+            if (!MST[w] && v->W < distance[w]) {
+                distance[w] = v->W;
+                nearest[w] = u;
+                decrease_key(heap, w, distance);
             }
         }
     }
+
+    // 최소 신장 트리의 결과를 출력합니다.
     printf("\n");
-    printf("시작 정점: %d\n", start);
+    printf("----------------최종 가중치 출력----------------\n\n");
+    printf("시작 정점은 %d\n", start);
     for (int i = 0; i < g->a; i++) {
-        if (i != start && distance[i] != INT_MAX) {
+        if (i != start && distance[i] != INF) {
             printf("정점 %d에서 %d | 가중치: %d\n", nearest[i], i, distance[i]);
-            totalWeight += distance[i];
+            totalWeight += distance[i]; // 총 가중치를 계산합니다.
         }
     }
     printf("\n");
-    printf("최소 가중치 값은 %d이다.\n", totalWeight);  // 총 가중치 출력
+    printf("최소 가중치 값은 %d이다.\n", totalWeight);  // 총 가중치를 출력합니다.
 }
-
-
 
 //main 함수
 int main() {
     inputGraph* g;
-    g = (inputGraph*)malloc(sizeof(inputGraph));
-    
-    Heap* h = init_Heap();
-    init_adjList(g);
+    g = (inputGraph*)malloc(sizeof(inputGraph)); // 그래프를 위한 메모리 할당
 
+    Heap* h = init_Heap(); // 힙 초기화
+    init_adjList(g); // 인접 리스트 초기화
+
+    // 그래프에 정점들을 추가합니다.
     for (int i = 0; i < 9; i++) {
         add_vertex(g, i);
     }
+    // 그래프에 간선들을 추가합니다. 각 간선은 두 정점과 가중치를 가집니다.
     add_edge(g, 0, 1, 4);
     add_edge(g, 0, 7, 8);
     add_edge(g, 1, 2, 8);
@@ -230,8 +263,9 @@ int main() {
     add_edge(g, 6, 7, 1);
     add_edge(g, 6, 8, 6);
     add_edge(g, 7, 8, 7);
-    print_adjList(g);
+    print_adjList(g); // 인접 리스트를 이용한 그래프 출력
 
+    // 힙에 모든 정점을 무한대 가중치와 함께 추가합니다.
     add_heap(h, 0, INF);
     add_heap(h, 1, INF);
     add_heap(h, 2, INF);
@@ -243,7 +277,7 @@ int main() {
     add_heap(h, 8, INF);
 
 
-    Prim(g, 0);
+    Prim(g, 0); // 프림 알고리즘을 시작 정점 0부터 실행합니다.
    
     return 0;
 }
